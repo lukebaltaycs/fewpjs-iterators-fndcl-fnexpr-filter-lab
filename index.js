b99ba96f60a5c1d538895f0b7e07b924a1a80321
Luke Baltay <luke.baltay@yale.edu>
2022-07-18 13:47:05 -0400

    commit tree 498ed254313013b04c2419e43260ed36aedbcd54

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution here
+
+function findMatching(drivers, name) {
+    return drivers.filter(n => {
+        return n.toUpperCase() === name.toUpperCase();
+    });
+}
+
+function fuzzyMatch(drivers, name) {
+    return drivers.filter(n => {
+        return n.substring(0, name.length) === name;
+    });
+}
+
+function matchName(drivers, name) {
+    return drivers.filter(n => {
+        return n.name === name;
+    });
+}
